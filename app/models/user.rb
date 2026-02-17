0123c7a72a5e4c5f0c1305b62578599eebed8067
maco_green <version.k0824@gmail.com>
2026-02-17 14:25:13 +0900

    ユーザーのアイコン画像アップロード機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :icon
+
+  validate :icon_content_type
+
+  private
+
+  def icon_content_type
+    return unless icon.attached?
+    return if icon.blob.content_type.in?(%w[image/jpeg image/png image/gif])
+
+    errors.add(:icon, 'はJPEG/PNG/GIFのみアップロードできます')
+  end
 end

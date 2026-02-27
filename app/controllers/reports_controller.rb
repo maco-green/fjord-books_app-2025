27a3ad444c41a30def34d23c35c1ea8e6aec9d4c
maco_green <version.k0824@gmail.com>
2026-02-27 17:45:07 +0900

    scaffoldの自動生成・不要なJSONファイルの削除・日報の権限変更

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,65 @@
+class ReportsController < ApplicationController
+  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :correct_user, only: %i[edit update destroy]
+
+  # GET /reports
+  def index
+    @reports = Report.all
+  end
+
+  # GET /reports/1
+  def show
+  end
+
+  # GET /reports/new
+  def new
+    @report = Report.new
+  end
+
+  # GET /reports/1/edit
+  def edit
+  end
+
+  # POST /reports
+  def create
+    @report = Report.new(report_params)
+    @report.user = current_user
+
+    if @report.save
+      redirect_to @report, notice: "Report was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /reports/1
+  def update
+    if @report.update(report_params)
+      redirect_to @report, notice: "Report was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /reports/1
+  def destroy
+    @report.destroy!
+    redirect_to reports_path, status: :see_other, notice: "Report was successfully destroyed."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_report
+      @report = Report.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def report_params
+      params.expect(report: [ :title, :body ])
+    end
+
+    def correct_user
+      @report = Report.find(params[:id])
+      redirect_to root_path unless @report.user == current_user
+    end
+end

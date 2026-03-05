758bb8c5cb7a625a6c79ff04beb6fdc76a9e676c
maco_green <version.k0824@gmail.com>
2026-03-05 17:26:39 +0900

    コメントの編集機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@
 
 class CommentsController < ApplicationController
   before_action :set_commentable
-  before_action :correct_user, only: %i[destroy]
+  before_action :set_comment, only: %i[edit update destroy]
+  before_action :correct_user, only: %i[edit update destroy]
 
   def create
     @comment = Comment.new(comment_params)
@@ -16,6 +17,16 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
@@ -23,6 +34,10 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
   def comment_params
     params.expect(comment: [:body])
@@ -37,7 +52,6 @@ class CommentsController < ApplicationController
   end
 
   def correct_user
-    @comment = Comment.find(params[:id])
     redirect_to @commentable, notice: t('controllers.comments.correct_user.notice') unless @comment.user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy, :edit, :update]
   end
   resources :users, only: %i(index show)
   resources :reports do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy, :edit, :update]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

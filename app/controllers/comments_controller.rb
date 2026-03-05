c24ced99eb2a7561ef947b30a01c64f80a548aa2
maco_green <version.k0824@gmail.com>
2026-03-05 15:07:33 +0900

    コメントの空送信防止（モデルバリデーション・HTML5必須チェック追加）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
-      render :new, status: :unprocessable_entity
+      redirect_to @commentable
     end
   end
 

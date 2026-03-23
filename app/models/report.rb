5ab5f509493286effd4ee22c4068db3208414edf
maco_green <version.k0824@gmail.com>
2026-03-23 15:42:50 +0900

    日報のtitleとbodyにバリデーション追加およびエラー時の文言のi18n化

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,6 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  validates :body, presence: true
+  validates :title, presence: true
 end

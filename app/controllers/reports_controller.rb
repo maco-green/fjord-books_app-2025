925e0d80ca2b1012d03fd8e20fc7ece78b6743a0
maco_green <version.k0824@gmail.com>
2026-04-07 14:09:40 +0900

    createアクションで関連を使いシンプルに変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,8 +23,7 @@ class ReportsController < ApplicationController
 
   # POST /reports
   def create
-    @report = Report.new(report_params)
-    @report.user = current_user
+    @report = current_user.reports.new(report_params)
 
     if @report.save
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)

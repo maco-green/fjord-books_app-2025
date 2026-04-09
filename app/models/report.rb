95261515c4f3083e5a5b2a8693951432b58c51ca
maco_green <version.k0824@gmail.com>
2026-04-09 17:24:31 +0900

    view表示追加とモデルのオプション修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,10 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :mentioning_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id
-  has_many :mentioned_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id
-  has_many :mentioning_reports, through: :mentioning_mentions, source: :mentioning_report
-  has_many :mentioned_reports, through: :mentioned_mentions, source: :mentioned_report
+  has_many :mentioning_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioning_report
+  has_many :mentioned_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned_reports, through: :mentioned_mentions, source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -20,12 +19,12 @@ class Report < ApplicationRecord
   end
 
   def find_report_ids
-    content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/).flatten.map(&:to_i)
+    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
   end
 
   def update_mentions
     find_report_ids.each do |report_id|
-      Mention.create(mentioning_report_id: self.id, mentioned_report_id: report_id)
+      Mention.create(mentioning_report_id: id, mentioned_report_id: report_id)
     end
   end
 end

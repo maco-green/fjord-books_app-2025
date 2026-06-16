9811eb85f0a612e7fe245b05ebc41250af7dd29d
maco_green <version.k0824@gmail.com>
2026-06-16 15:27:35 +0900

    name_or_emailのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "nameがあればnameを返す" do
+    user = users(:one)
+
+    assert_equal 'namae', user.name_or_email
+  end
+
+  test "nameがなければemailを返す" do
+    user = users(:two)
+
+    assert_equal 'two@example.com', user.name_or_email
+  end
 end

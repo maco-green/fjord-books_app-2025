3bae47bb24cf39ef49f5060a98158b7c1048f298
maco_green <version.k0824@gmail.com>
2026-06-19 15:59:03 +0900

    Rubocop指摘分の修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "nameがあればnameを返す" do
+  test 'nameがあればnameを返す' do
     user = users(:one)
 
     assert_equal 'namae', user.name_or_email
   end
 
-  test "nameがなければemailを返す" do
+  test 'nameがなければemailを返す' do
     user = users(:two)
 
     assert_equal 'two@example.com', user.name_or_email

f4699762475cf4c96c5b94d1161f39cae5df172f
maco_green <version.k0824@gmail.com>
2026-06-29 14:20:20 +0900

    レビュー指摘内容の修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -3,12 +3,14 @@
 require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
+  LOGIN_PASSWORD = 'password'
+
   setup do
     @report = reports(:one)
 
     visit new_user_session_path
     fill_in 'Eメール', with: 'one@example.com'
-    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード', with: LOGIN_PASSWORD
     click_button 'ログイン'
     assert_text 'ログインしました。', wait: 10
   end
@@ -22,11 +24,14 @@ class ReportsTest < ApplicationSystemTestCase
     visit reports_url
     click_link href: new_report_path
 
-    fill_in '内容', with: @report.content
-    fill_in 'タイトル', with: @report.title
+    fill_in '内容', with: '作成テストの内容'
+    fill_in 'タイトル', with: '作成テストのタイトル'
 
     click_on '登録する'
     assert_text '日報が作成されました。'
+
+    assert_text '作成テストのタイトル'
+    assert_text '作成テストの内容'
   end
 
   test 'should update Report' do
@@ -38,6 +43,9 @@ class ReportsTest < ApplicationSystemTestCase
     click_button '更新する'
 
     assert_text '日報が更新されました。', wait: 10
+
+    assert_text 'タイトル変更後'
+    assert_text '内容変更後'
   end
 
   test 'should destroy Report' do
@@ -45,5 +53,8 @@ class ReportsTest < ApplicationSystemTestCase
     click_on 'この日報を削除'
 
     assert_text '日報が削除されました。'
+
+    assert_no_text @report.title
+    assert_no_text @report.content
   end
 end

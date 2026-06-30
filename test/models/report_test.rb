26a656411482bbe3bb7222cca70e55ed45dcd28d
maco_green <version.k0824@gmail.com>
2026-06-30 17:41:56 +0900

    メンション更新時のテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -24,13 +24,29 @@ class ReportTest < ActiveSupport::TestCase
     assert_equal Date.new(2026, 1, 1), report.created_on
   end
 
-  test '日報本文に含まれる日報URLをメンションとして保存する' do
+  test '日報本文に含まれる日報URLに応じてメンションを更新する' do
     mentioning_report = reports(:one)
-    mentioned_report = reports(:two)
+    removed_report = reports(:two)
+    kept_report = reports(:three)
+    added_report = reports(:four)
 
-    mentioning_report.content = "http://localhost:3000/reports/#{mentioned_report.id}"
+    mentioning_report.content = [
+      "http://localhost:3000/reports/#{removed_report.id}",
+      "http://localhost:3000/reports/#{kept_report.id}"
+    ].join("\n")
     mentioning_report.save!
 
-    assert_includes mentioning_report.reload.mentioning_reports, mentioned_report
+    assert_includes mentioning_report.reload.mentioning_reports, removed_report
+    assert_includes mentioning_report.mentioning_reports, kept_report
+
+    mentioning_report.content = [
+      "http://localhost:3000/reports/#{kept_report.id}",
+      "http://localhost:3000/reports/#{added_report.id}"
+    ].join("\n")
+    mentioning_report.save!
+
+    assert_not_includes mentioning_report.reload.mentioning_reports, removed_report
+    assert_includes mentioning_report.mentioning_reports, kept_report
+    assert_includes mentioning_report.mentioning_reports, added_report
   end
 end
